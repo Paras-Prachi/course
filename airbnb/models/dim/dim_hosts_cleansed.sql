114fd8cbd8cead164869dcbb0c1de32d660814a6
Paras-Prachi <PrachiPidi.Paras@cognizant.com>
2026-04-16 11:46:43 +0530

    dim_listing,dim_hosts models created

---
+++ airbnb/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,14 @@
+with src_host as (
+    select * from {{ ref('src_host') }}
+)
+select  
+    host_id,
+    nvl(
+        host_name,
+        'Anonymous'
+    ) as host_name,
+    IS_SUPERHOST,
+    created_at,
+    updated_at
+from
+    src_host
